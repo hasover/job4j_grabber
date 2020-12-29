f182217dd2ff54f37f640daa06c43f2a284ce5e3
hasover <hasover@gmail.com>
2020-12-30 00:09:50 +0300

    2. Парсинг HTML страницы. [#389311]

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,24 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        Elements dates = doc.select(".altCol");
+        int index = 1;
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            System.out.println(dates.get(index).text());
+            index += 2;
+        }
+    }
+}
