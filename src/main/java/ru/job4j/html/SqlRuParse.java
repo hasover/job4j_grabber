c853aac4d447a265029651f0080cd797999a5fe3
hasover <hasover@gmail.com>
2020-12-30 22:17:39 +0300

    2.1. Преобразование даты [#389319]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,9 +6,55 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SqlRuParse {
-    public static void main(String[] args) throws IOException {
+    public static String formatDate(String strToFormat) throws ParseException {
+
+        String[] strData = strToFormat.trim().split(",\\s");
+        String time = strData[1];
+        String dateToFormat = strData[0];
+
+        String formattedDate = "";
+        if (dateToFormat.equals("сегодня")) {
+            formattedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        } else if (dateToFormat.equals("вчера")) {
+            formattedDate = LocalDate.now().minusDays(1).
+                    format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        } else {
+            String[] dateInfo = dateToFormat.trim().split("\\s");
+
+            if (dateInfo[0].trim().length() == 1) {
+                formattedDate += "0" + dateInfo[0];
+            } else {
+                formattedDate += dateInfo[0];
+            }
+
+            switch (dateInfo[1]) {
+                case "янв" : formattedDate += "-01"; break;
+                case "фев" : formattedDate += "-02"; break;
+                case "мар" : formattedDate += "-03"; break;
+                case "апр" : formattedDate += "-04"; break;
+                case "май" : formattedDate += "-05"; break;
+                case "июн" : formattedDate += "-06"; break;
+                case "июл" : formattedDate += "-07"; break;
+                case "авг" : formattedDate += "-08"; break;
+                case "сен" : formattedDate += "-09"; break;
+                case "окт" : formattedDate += "-10"; break;
+                case "ноя" : formattedDate += "-11"; break;
+                case "дек" : formattedDate += "-12"; break;
+                default: break;
+            }
+
+            formattedDate += "-20" + dateInfo[2];
+        }
+
+        return formattedDate + " " + time;
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
         Elements dates = doc.select(".altCol");
@@ -17,7 +63,7 @@ public class SqlRuParse {
             Element href = td.child(0);
             System.out.println(href.attr("href"));
             System.out.println(href.text());
-            System.out.println(dates.get(index).text());
+            System.out.println(formatDate(dates.get(index).text()));
             index += 2;
         }
     }
