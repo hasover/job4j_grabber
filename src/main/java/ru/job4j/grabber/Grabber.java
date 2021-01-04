64f8bb5a0a7a3fffdcd2682e22273fa59eed0bae
hasover <hasover@gmail.com>
2021-01-04 20:12:35 +0300

    7. Web. [#389321]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -4,6 +4,9 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import ru.job4j.html.SqlRuParse;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Properties;
 
 public class Grabber implements Grab {
@@ -60,11 +63,32 @@ public class Grabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(
+                    Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes());
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+
+            }
+        }).start();
+    }
+
     public static void main(String[] args) throws SchedulerException {
         Grabber grabber = new Grabber();
         grabber.cfg();
         Scheduler scheduler = grabber.scheduler();
         Store store = grabber.store();
         grabber.init(new SqlRuParse(), store, scheduler);
+        grabber.web(store);
     }
 }
